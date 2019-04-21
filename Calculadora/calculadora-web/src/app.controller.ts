9d5615be2e6806a56e26ed91d6ecd0c373fd2a0f
jsofii <sofi-0106@hotmail.com>
2019-04-20 20:35:29 -0500

    Deber Terminado

--- Calculadora/calculadora-web/src/app.controller.ts
+++ Calculadora/calculadora-web/src/app.controller.ts
@@ -1,4 +1,4 @@
-import {Controller, Get,Headers, Put,Delete,Response, HttpCode, Post} from '@nestjs/common';
+import {Controller, Get, Headers, Put, Delete, Response, HttpCode, Post, Body, Query, Header} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller('/api')
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/suma')
-  @HttpCode(201)
+  @HttpCode(200)
   suma(@Headers() headers, @Response() response){
     const num1=Number(headers.num1)
     const num2=Number(headers.num2)
@@ -20,6 +20,54 @@ export class AppController {
       })
     }
   }
-  @Post('/')
+  @Post('/resta')
+  @HttpCode(201)
+  resta(@Body() body, @Response() response){
+    const num1=Number(body.num1)
+    const num2=Number(body.num2)
+    if(num1 && num2) {
+      return response.send({
+        resta: num1 - num2
+      });
+    }else{
+      return response.send({
+        error: 'Datos no válidos'
+      })
+    }
+  }
+
+  @Put('/multiplicacion')
+  @HttpCode(202)
+    multiplicacion(@Query() query, @Response() response){
+      const num1=Number(query.num1)
+      const num2=Number(query.num2)
+      if(num1 && num2) {
+        return response.send({
+          multiplicacion: num1 * num2
+        });
+      }else{
+        return response.send({
+          error: 'Datos no válidos'
+        })
+      }
+
+    }
+  @Delete('/division')
+  @HttpCode(203)
+  division(@Query() query, @Headers() header, @Response() response){
+    const num1=Number(query.num1)
+    const num2=Number(header.num2)
+    if(num1 && num2 && num2!=0) {
+      return response.send({
+        division: num1 / num2
+      });
+    }else{
+      return response.send({
+        error: 'Datos no válidos'
+      })
+    }
+
+  }
+
 
 }
